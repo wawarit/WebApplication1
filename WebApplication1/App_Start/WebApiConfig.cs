15a995fff66815a08f305afe7225c49bf1574637
Waritsara NONTALERT <waritsara.no@live.ku.th>
2023-04-03 20:20:54 +0700

    แก้ไข

--- WebApplication1/App_Start/WebApiConfig.cs
+++ WebApplication1/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WebApplication1
 {
@@ -19,6 +20,8 @@ namespace WebApplication1
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
---
+++ WebApplication1/Controllers/CalendarController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class CalendarController : Controller
+    {
+        public class Day
+        {
+            public string Holiday { get; set; }
+            public string data { get; set; }
+
+            //public string Name { get; set; }
+        }
+
+        public static string sqlConn = ConfigurationManager.ConnectionStrings["Project"].ToString();
+        private string sql;
+        
+        // GET: Calendar
+        public ActionResult Index(Day data)
+        {
+            DataTable dtable = new DataTable();
+            
+
+            using (SqlConnection con = new SqlConnection(sqlConn))
+            {
+                con.Open();
+
+
+                using (var cmd = new SqlCommand(sql, con))
+                {
+
+                    var reader = cmd.ExecuteReader();
+                    dtable.Load(reader);
+                    con.Close();
+                }
+            }
+            return View();
+        }
+
+
+
+    }
+}
